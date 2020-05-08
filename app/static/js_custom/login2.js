2a8d970ba2dd867cafec1b6fe70dded3c8070e24
mvalkhimovich <mvalkhimovich@miem.hse.ru>
2020-05-09 02:19:49 +0300

    new login2.js without prevent default submit form method

---
+++ app/static/js_custom/login2.js
@@ -0,0 +1,32 @@
+document.getElementById("btnsubmit").onclick = function() {myFunction()};
+
+function myFunction() {
+    $("#btnsubmit").attr("disabled", true);
+    $.ajax({
+        url: "/api/login",
+        type: "POST",
+        data: JSON.stringify({"email": document.getElementById("login_user_email").value,
+                              "password": document.getElementById("login_user_password").value
+                             }),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data){
+            $('#message').html(data["description"]);
+            $("#f_login").trigger('reset');
+            setTimeout(
+                function() {
+                    window.location = '/';
+                }, 1000
+            )
+        },
+        error: function(data){
+            $('#message').html(data.responseJSON['error']);
+            $("#btnsubmit").attr("disabled", false);
+            setTimeout(
+                function() {
+                    $('#message').html("");
+                }, 3000
+            )
+        }
+    });
+}
