5a906e2ec441e8c683522e2200bff76508b6b875
Майя Кой <mddaneliya@miem.hse.ru>
2020-05-10 17:27:29 +0300

    register in progress

---
+++ app/static/js_custom/register.js
@@ -0,0 +1,34 @@
+document.getElementById("btnsubmit").onclick = function() {myFunction()};
+
+function myFunction() {
+    $("#btnsubmit").attr("disabled", true);
+    $.ajax({
+        url: "/api/register",
+        type: "POST",
+        data: JSON.stringify({"email": document.getElementById("register_user_email").value,
+                              "password": document.getElementById("register_user_password").value,
+                              "name": document.getElementById("register_user_name").value,
+                              "surname": document.getElementById("register_user_surname").value
+                             }),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data){
+            $('#message').html(data["description"]);
+            $("#f_register").trigger('reset');
+            setTimeout(
+                function() {
+                    window.location = '/';
+                }, 1000
+            )
+        },
+        error: function(data){
+            $('#message').html(data.responseJSON['error']);
+            $("#btnsubmit").attr("disabled", false);
+            setTimeout(
+                function() {
+                    $('#message').html("");
+                }, 3000
+            )
+        }
+    });
+}
