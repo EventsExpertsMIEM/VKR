55c96d4836b786ddc5bf8fb4464d4e99cba2be12
vagrant <lnkryukov@miem.hse.ru>
2020-05-13 14:46:59 +0000

    proper js

--- app/static/js_custom/create_event.js
+++ app/static/js_custom/create_event.js
@@ -1,53 +1,41 @@
-$(function() {
-    create_event_start_date.min = new Date().toISOString().split("T")[0];
-    create_event_end_date.min = new Date().toISOString().split("T")[0];
-    $("#f_create_event").submit(function(e) {
-        e.preventDefault();
-        $("#btnsubmit_create_event").attr("disabled", true);
-        var name = document.getElementById("create_event_name").value;
-        var start_date = document.getElementById("create_event_start_date").valueAsDate.toISOString().split("T")[0];
-        var end_date = document.getElementById("create_event_end_date").valueAsDate.toISOString().split("T")[0];
-        var start_time = document.getElementById("create_event_start_time").value;
-        var loc = document.getElementById("create_event_location").value;
-        var site_link = document.getElementById("create_event_site_link").value;
-        var sm_description = document.getElementById("create_event_description").value;
-        var description = document.getElementById("create_event_sm_description").value;
-        var additional_info = document.getElementById("create_event_additional_info").value;
-        $.ajax({
-            url: "/api/event",
-            type: "POST",
-            data: JSON.stringify({"name" : name,
-                                  "start_date" : start_date,
-                                  "end_date" : end_date,
-                                  "start_time" : start_time,
-                                  "location" : loc,
-                                  "site_link" : site_link,
-                                  "sm_description" : sm_description,
-                                  "description" : description,
-                                  "additional_info" : additional_info
-                                }),
-            contentType: "application/json",
-            dataType: "json",
-            success: function(data){
-                $('#message').html(data["description"]);
-                $("#f_create_event").trigger('reset');
-                setTimeout(
-                    function() {
-                        $("#btnsubmit_create_event").html("Отправить");
-                        $("#btnsubmit_create_event").attr("disabled", false);
-                        window.location = '/';
-                    }, 1000
-                )
-            },
-            error: function(data){
-                $('#message').html(data.responseJSON['error']);
-                $("#btnsubmit_create_event").attr("disabled", false);
-                setTimeout(
-                    function() {
-                        $('#message').html("");
-                    }, 3000
-                )
+document.getElementById('btnsubmit_create_event').addEventListener(
+    'click',
+    () => {
+        var data = {
+            name: document.getElementById("create_event_name").value,
+            start_date: document.getElementById("create_event_start_date").value,
+            end_date: document.getElementById("create_event_end_date").value,
+            start_time: document.getElementById("create_event_start_time").value,
+            location: document.getElementById("create_event_location").value,
+            site_link: document.getElementById("create_event_site_link").value,
+            sm_description: document.getElementById("create_event_description").value,
+            description: document.getElementById("create_event_sm_description").value,
+            additional_info: document.getElementById("create_event_additional_info").value
+        }
+        fetch("/api/event",
+            {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
             }
-        });
-    });
-});
+        ).then(
+            response => {
+                if (response.status >= 200 && response.status < 300) {
+                    response.json().then(
+                        data => {
+                            var id = data['description'] // id созданного мероприятия
+                            window.location.href = `/event/${id}` // перенаправление на страницу
+                        }
+                    )
+                } else {
+                    console.log(response) // TODO: Error handling
+                }
+            }
+        ).catch(
+            e => console.log(e) // TODO: Error handling
+        )
+    }
+)
