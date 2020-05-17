d7b2e2fd39b5a3196a94017201449f2d0b63e7e5
Лев Крюков <lnkryukov@miem.hse.ru>
2020-05-17 16:23:36 +0300

    registration form refactoring

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,35 +1,94 @@
-$(function() {
-    $("#f_register").submit(function(e) {
-        e.preventDefault();
-        $("#btnsubmit").attr("disabled", true);
-        $.ajax({
-            url: "/api/register",
-            type: "POST",
-            data: JSON.stringify({"email": document.getElementById("register_user_email").value,
-                                  "password": document.getElementById("register_user_password").value,
-                                  "name": document.getElementById("register_user_name").value,
-                                  "surname": document.getElementById("register_user_surname").value
-                                }),
-            contentType: "application/json",
-            dataType: "json",
-            success: function(data){
-                $("#btnsubmit").html(data["description"]);
-                $("#f_register").trigger('reset');
-                setTimeout(
-                    function() {
-                        window.location = '/';
-                    }, 1000
-                )
-            },
-            error: function(data){
-                $("#btnsubmit").html(data.responseJSON['error']);
-                $("#btnsubmit").attr("disabled", false);
-                setTimeout(
-                    function() {
-                        $("#btnsubmit").html("Зарегистрироваться");
-                    }, 3500
+document.addEventListener('DOMContentLoaded', () => {
+    
+    var form = document.getElementById('registration_form')
+    var button = document.getElementById('registration_form_submit_button')
+
+    form.addEventListener('input', () => {
+
+        var passwordInput = document.getElementById('register_user_password')
+        var passwordInputConfirmation =
+            document
+                .getElementById('register_user_password_confirmation')
+        
+        if (passwordInput.value != passwordInputConfirmation.value) {
+            passwordInputConfirmation.setCustomValidity('Пароли не совпадают')
+        } else {
+            passwordInputConfirmation.setCustomValidity('')
+        }
+        
+
+    })
+
+    form.addEventListener('submit', event => {
+
+        event.preventDefault()
+
+        button.disabled = true;
+
+        fetch(
+            '/api/register',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        email:
+                            document
+                                .getElementById('register_user_email')
+                                    .value,
+                        password:
+                            document
+                                .getElementById('register_user_password')
+                                    .value,
+                        name:
+                            document
+                                .getElementById('register_user_name')
+                                    .value,
+                        surname: 
+                            document
+                                .getElementById('register_user_surname')
+                                    .value
+                    }
                 )
             }
-        });
-    });
-});
+        ).catch(
+            error => console.log(error)
+        ).then(
+            response => {
+                if (response.status < 200 || response.status >= 300) {
+                    response.json().then(
+                        body => {
+                            button.textContent = body['error']
+                            setTimeout(
+                                () => {
+                                    button.disabled = false
+                                    button.textContent = 'Отправить'
+                                },
+                                750
+                            )
+                        }
+                    )
+                    return Promise.reject('Request finished with error')
+                }
+                return response.json()
+            }
+        ).then(
+            body => {
+                button.textContent = body['description']
+                setTimeout(
+                    () => {
+                        button.disabled = false
+                        button.textContent = 'Отправить'
+                        window.location = '/'
+                    },
+                    750
+                )
+            }
+        ).catch(
+            error => console.log(error)
+        )
+
+    })
+})
