ca58bea4cbe374ab591c97bd4da737b9269b11e7
Лев Крюков <lnkryukov@miem.hse.ru>
2020-05-17 14:09:49 +0300

    redirect after login

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,33 +1,61 @@
-$(function() {
-    $("#f_login").submit(function(e) {
-        e.preventDefault();
-        $("#btnsubmit").attr("disabled", true);
-        $.ajax({
-            url: "/api/login",
-            type: "POST",
-            data: JSON.stringify({"email": document.getElementById("login_user_email").value,
-                                  "password": document.getElementById("login_user_password").value
-                                }),
-            contentType: "application/json",
-            dataType: "json",
-            success: function(data){
-                $("#btnsubmit").html(data["description"]);
-                $("#f_login").trigger('reset');
-                setTimeout(
-                    function() {
-                        window.location = '/';
-                    }, 1000
-                )
-            },
-            error: function(data){
-                $("#btnsubmit").html(data.responseJSON['error']);
-                $("#btnsubmit").attr("disabled", false);
-                setTimeout(
-                    function() {
-                        $("#btnsubmit").html("Войти");
-                    }, 3500
-                )
+document.addEventListener('DOMContentLoaded', () => {
+
+    var form = document.getElementById('login_form')
+    var button = document.getElementById('login_submit_button')
+
+    form.addEventListener('submit', event => {
+
+        event.preventDefault()
+
+        var responsePromise = fetch(
+            '/api/login',
+            {
+                method: 'POST',
+                body: JSON.stringify(
+                    {
+                        email: document.getElementById('login_user_email').value,
+                        password: document.getElementById('login_user_password').value
+                    }
+                ),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             }
-        });
-    });
-});
+        )
+
+        responsePromise = responsePromise.then(
+            response => {
+                if (response.status < 200 || response.status >= 300) {
+                    button.disabled = true
+                    response.json().then(
+                        data => {
+                            button.textContent = data['error']
+                            setTimeout(
+                                () => {
+                                    button.disabled = false
+                                    button.textContent = 'Отправить'
+                                },
+                                750
+                            )
+                        }
+                    )
+                    return
+                }
+                return response.json()
+            }
+        )
+        responsePromise.catch(
+            error => console.log(error)
+        )
+
+        responsePromise.then(
+            data => {
+                const urlParams = new URLSearchParams(window.location.search);
+                const location = urlParams.get('next');
+                window.location = location
+            }
+        )
+
+    })
+
+})
