99453f5ad350bb3be5f8e3fd0f6a8255ca602707
Майя Кой <mddaneliya@miem.hse.ru>
2020-05-26 17:40:18 +0300

    collapse margin

--- app/static/js/event_search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#event_name_search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".event_card .card-header").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
