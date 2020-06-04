ce565b59f4cf4c555c69957d0220b8d86c4b5336
Лев Крюков <lnkryukov@miem.hse.ru>
2020-06-04 13:18:57 +0000

    tags :crtbls

--- app/static/js/admin/tag.js
+++ app/static/js/admin/tag.js
@@ -3,18 +3,99 @@ export { createTag, editTag, deleteTag }
 
 function createTag(name) {
 
-    console.log(`Create tag ${name}`)
+    fetch(
+        '/api/tag',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    name: name
+                }
+            )
+        }
+    ).then(
+        response => {
+            if (response.status != 201) {
+                return response.json().then(
+                    json_body => Promise.reject(
+                        {
+                            code: response.status,
+                            message: json_body.error
+                        }
+                    )
+                )
+            }
+        }
+    ).then(
+        json_body => window.location = ''
+    ).catch(
+        error => console.log(error)
+    )
 
 }
 
 function editTag(id, newName) {
 
-    console.log(`Edit tag ${id} ${newName}`)
+    fetch(
+        `/api/tag/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    name: newName
+                }
+            )
+        }
+    ).then(
+        response => {
+            if ( response.status != 200 ) {
+                return response.json().then(
+                    json_body => Promise.reject(
+                        {
+                            code: response.status,
+                            message: json_body.error
+                        }
+                    )
+                )
+            }
+        }
+    ).then(
+        json_body => window.location = ''
+    ).catch(
+        error => console.log(error)
+    )
 
 }
 
 function deleteTag(id) {
 
-    console.log(`Delete tag ${id}`)
-
+    fetch(
+        `/api/tag/${id}`,
+        {
+            method: 'DELETE'
+        }
+    ).then(
+        response => {
+            if ( response.status != 200 ) {
+                return response.json().then(
+                    json_body => Promise.reject(
+                        {
+                            code: response.status,
+                            message: json_body.error
+                        }
+                    )
+                )
+            }
+        }
+    ).then(
+        json_body => window.location = ''
+    ).catch(
+        error => console.log(error)
+    )
 }
