2cee3f40540649520dd35efd3c05e519fcc79cd0
Лев Крюков <lnkryukov@miem.hse.ru>
2020-06-10 00:08:49 +0000

    oauth

--- app/web/oauth.py
+++ app/web/oauth.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, url_for, request, redirect, jsonify
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 import requests
 import logging
@@ -53,6 +53,28 @@ def vk_login_callback():
     return redirect(url_for('events_web.home'))
 
 
+@bp.route('/vk/link')
+def vk_link():
+    vcunt = app.oauth.vk
+    redirect_uri = url_for('oauth.vk_link_callback', _external=True)
+    return vcunt.authorize_redirect(redirect_uri)
+
+@bp.route('/vk/link/callback')
+def vk_link_callback():
+    token = app.oauth.vk.authorize_access_token()
+    logging.info("Register: got oauth token: {}".format(token))
+    resp = requests.get(
+        'https://api.vk.com/method/users.get',
+        params = {
+            'v': '5.103',
+            'access_token': token['access_token']
+        }
+    ).json()['response'][0] # TODO: Error checks, cleanup
+    logging.info("Got user data: {}".format(resp))
+    accounts_logic.add_oauth_info(current_user.email, token['user_id'], 'vk')
+    return redirect(url_for('users_web.profile'))
+
+
 @bp.route('google/register')
 def google_register():
     google = app.oauth.google
@@ -101,3 +123,26 @@ def google_login_callback():
     user = accounts_logic.oauth_pre_login(resp['id'])
     login_user(user)
     return redirect(url_for('events_web.home'))
+
+    
+
+@bp.route('/google/link')
+def google_link():
+    google = app.oauth.google
+    redirect_uri = url_for('oauth.google_link_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@bp.route('/google/link/callback')
+def google_link_callback():
+    token = app.oauth.google.authorize_access_token()
+    logging.getLogger(__name__).debug("Register: got oauth token: {}".format(token))
+    resp = requests.get(
+         'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
+        headers = {
+            'Authorization': 'Bearer {}'.format(token['access_token'])
+        }
+    ).json()
+    logging.getLogger(__name__).debug("Google user info: {}".format(resp))
+    accounts_logic.add_oauth_info(current_user.email, resp['id'], 'google')
+    return redirect(url_for('users_web.profile'))
